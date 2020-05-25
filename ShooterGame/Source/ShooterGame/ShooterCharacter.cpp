6e0388d024164ca550140c968811ad818f13a28f
LeoFoxCruz <leonardorbc@gmail.com>
2020-05-25 16:54:54 -0400

    added movement, camera code. Resized scale and camera view of character.

--- ShooterGame/Source/ShooterGame/ShooterCharacter.cpp
+++ ShooterGame/Source/ShooterGame/ShooterCharacter.cpp
@@ -3,32 +3,39 @@
 
 #include "ShooterCharacter.h"
 
-// Sets default values
 AShooterCharacter::AShooterCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
-// Called when the game starts or when spawned
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
+
 void AShooterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForBackwards"), this, &AShooterCharacter::MoveForBackwards);
+	PlayerInputComponent->BindAxis(TEXT("MoveSideways"), this, &AShooterCharacter::MoveSideways);
+	PlayerInputComponent->BindAxis(TEXT("LookUpDown"), this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("LookSideways"), this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent:: IE_Pressed, this, &ACharacter::Jump);
 }
 
+void AShooterCharacter::MoveForBackwards(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void AShooterCharacter::MoveSideways(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
--- ShooterGame/Source/ShooterGame/ShooterCharacter.h
+++ ShooterGame/Source/ShooterGame/ShooterCharacter.h
@@ -12,18 +12,15 @@ class SHOOTERGAME_API AShooterCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AShooterCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	void MoveForBackwards(float AxisValue);
+	void MoveSideways(float AxisValue);
 
 };
